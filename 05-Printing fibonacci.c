29c4efbb24da7413dbcfc7323802696ec01fe2d8
SK <englishenglishenglish@proton.me>
2025-07-11 08:26:44 +0000

    Update 05-Printing fibonacci.c

--- 05-Printing fibonacci.c
+++ 05-Printing fibonacci.c
@@ -2,7 +2,10 @@
 int main(){
 
         int n;
+        //logic for taking input
         scanf("%d",&n);
+
+        
         int n1=0,n2=1,n3;
         if(n>=1){
             printf("%d  ",n1);
@@ -11,7 +14,7 @@ int main(){
             printf("%d  ",n2);
         }
 
-
+        //logic for output
         for(int i=2;i<n;i++){
             
             n3=n1+n2;
